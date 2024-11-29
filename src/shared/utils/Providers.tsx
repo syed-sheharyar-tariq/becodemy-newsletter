6fd7fc3443490f13738a8c1f5ebb817548519463
Syed Sheharyar Professional <syedsheharyarprofessional@gmail.com>
2024-11-29 21:53:39 +0500

    Finished Auth with Clerk

--- src/shared/utils/Providers.tsx
+++ src/shared/utils/Providers.tsx
@@ -1,21 +1,29 @@
-"use client";
+"use client"
 import { NextUIProvider } from "@nextui-org/react"
 import { usePathname } from "next/navigation"
-
+import { useUser } from "@clerk/nextjs"
 interface ProviderProps {
   children: React.ReactNode
 }
 
-export default function Providers({children}: ProviderProps) {
-    const pathname = usePathname()
+export default function Providers({ children }: ProviderProps) {
+  const pathname = usePathname()
+  const { isLoaded } = useUser()
+
+  if (!isLoaded) return null
   return (
     <NextUIProvider>
-        {pathname !== "/dashboard/new-email" && pathname !== "/" && pathname !== "sign-up" && pathname !== "subscribe" && pathname !== "sign-in" ? ( <div className="w-full flex">
-            <div className="w-[290px] h-screen overflow-y-scroll"></div>
+      {pathname !== "/dashboard/new-email" &&
+      pathname !== "/" &&
+      pathname !== "/sign-up" &&
+      pathname !== "/subscribe" &&
+      pathname !== "/sign-in" ? (
+        <div className="w-full flex">
+          <div className="w-[290px] h-screen overflow-y-scroll"></div>
         </div>
-    ) : ( 
-    <>{children}</>
-)}
+      ) : (
+        <>{children}</>
+      )}
     </NextUIProvider>
   )
 }
--- src/shared/widgets/header/Toolbar.tsx
+++ src/shared/widgets/header/Toolbar.tsx
@@ -1,14 +1,33 @@
 "use client"
 import { Button } from "@nextui-org/react"
 import Link from "next/link"
+import Image from "next/image"
+import { useUser } from "@clerk/nextjs"
 
 export default function Toolbar() {
+  const { user } = useUser()
   return (
     <>
-      <Button color="primary" className="text-lg">
+      <Button
+        color="primary"
+        className="text-lg">
         Start Trial
       </Button>
-      <Link href="/sign-up">Login</Link>
+      {user ? (
+        <>
+          <Link href={"/dashboard"}>
+            <Image
+              src={user?.imageUrl}
+              alt=""
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </Link>
+        </>
+      ) : (
+          <Link href={"/sign-in"}>Login</Link>
+      )}
     </>
   )
 }
